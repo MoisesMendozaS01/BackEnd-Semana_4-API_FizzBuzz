1fe501fa60810962f2f48f12009c6250109cecff
= <=>
2022-05-17 18:49:30 -0500

    Agregando prueba de unidad de la clase ExplorerController funcion getExplorersUsernamesByMission

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -4,5 +4,12 @@ describe('Unit Test for ExplorerController',()=>{
     test('Parte 1: Probando clase ExplorerController',()=>{
         const ExplorersByMission = ExplorerController.getExplorersByMission('node');
         expect(ExplorersByMission).not.toBeUndefined
+        expect(ExplorersByMission[0].mission).toBe('node')
     })
+    test('Parte 2: función getExplorersUsernamesByMission',()=>{
+        const ExplorerUsernames = ExplorerController.getExplorersUsernamesByMission('node')
+
+        expect(ExplorerUsernames).toContain('Woopa14')
+    })
+    
 })
